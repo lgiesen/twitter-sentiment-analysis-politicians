e61e405434f14afba4154a54d48902e34e30e7a6
Leo Giesen <contact@leogiesen.de>
2024-01-09 16:01:49 +0100

    :art: improve structure of code

--- main.py
+++ main.py
@@ -1,15 +1,69 @@
-def main():
-    database = r"C:\sqlite\db\pythonsqlite.db"
+import os
+import sqlite3
+import time
+from sqlite3 import Error
 
-    # create a database connection
-    conn = create_connection(database)
-    with conn:
-        print("1. Query task by priority:")
-        select_task_by_priority(conn, 1)
+def get_files(dir):
+    """ get all files from a directory 
+    that do not start with a '.'
+    :param dir: String
+    :return: List of Strings
+    """
+    return sorted([s for s in os.listdir(dir) if not s.startswith('.')])
 
-        print("2. Query all tasks")
-        select_all_tasks(conn)
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def db_select(conn, table, limit, select = '*', where = 'True'):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute(f'SELECT {select} FROM {table} WHERE {where} LIMIT {limit}')
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def export_data(data, path_without_filetype, pickle=True, csv=True):
+    """
+    export data in a pickle and csv format
+    :param data: DataFrame
+    :param path_without_filetype: String filepath without the extension
+    :param pickle: Bool if it should be exported as pickle
+    :param csv: Bool if it should be exported as csv 
+    :return: None
+    """
+    if pickle:
+        data.to_pickle(f'{path_without_filetype}.pkl')
+    if csv:
+        data.to_csv(f'{path_without_filetype}.csv')
+
+def get_data():
+    root = '/Volumes/Festplatte/data-UA/'
+    data_path = root + 'data/'
+    presidents = ['trump', 'johnson']
+    cities = ['Birmingham', 'LA', 'London', 'NYC']
+    years = [str(year) for year in range(2018, 2023)] # ['2018', '2019', '2020', '2021', '2022']
+    return (root, data_path, presidents, cities, years)
+
+# def main():
+    # call some functions
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
--- preprocessing.py
+++ preprocessing.py
@@ -11,43 +11,8 @@ import time
 from sqlite3 import Error
 
 import pandas as pd
+from main import create_connection, get_files, export_data, get_data
 
-
-def get_files(dir):
-    """ get all files from a directory 
-    that do not start with a '.'
-    :param dir: String
-    :return: List of Strings
-    """
-    return sorted([s for s in os.listdir(dir) if not s.startswith('.')])
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-def db_select(conn, table, limit, select = '*', where = 'True'):
-    """
-    Query all rows in the tasks table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute(f'SELECT {select} FROM {table} WHERE {where} LIMIT {limit}')
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
 
 def create_dataset(conn, leader_hashtags, president):
     """ create a dataset based on a database connection to the SQLite database
@@ -57,18 +22,15 @@ def create_dataset(conn, leader_hashtags, president):
     :return: DataFrame
     """
     # join sentiment and tweet table
-    # only select english tweets
     data_en_keywords = pd.read_sql_query(
         f"""
         SELECT * 
         FROM tweets 
         INNER JOIN sentiment 
         USING (item_number) 
-        WHERE (
-            lang == 'en'
-            AND
-            text LIKE '%{president}%'
-        );
+        INNER JOIN LIWC
+        USING (item_number) 
+        WHERE text LIKE '%{president}%';
         """, 
         conn)
     # alternatively: # cur.execute('SQL STATEMENT')
@@ -82,32 +44,19 @@ def create_dataset(conn, leader_hashtags, president):
 
     tweets_by_hashtags = pd.read_sql_query(f"""
                                         SELECT * 
-                                        FROM tweets INNER JOIN sentiment 
+                                        FROM tweets 
+                                        INNER JOIN sentiment 
+                                        USING (item_number) 
+                                        INNER JOIN LIWC
                                         USING (item_number) 
                                         WHERE (
-                                            lang == 'en' 
-                                            AND 
                                             item_number IN {str(tuple(newly_relevant_hashtags)).replace(',)',')')}
                                         )""", conn)
     # .replace(',)',')') because the tuple creates a comma at the end, which disrupts the SQL statement
-    print(f'There are {tweets_by_hashtags.shape[0]} tweets which are in english and are relevant by using the hashtag')
+    print(f'There are {tweets_by_hashtags.shape[0]} tweets which are relevant by using the hashtag')
     # concat the tweets from before with the relevant hashtag-tweets
     data_en_keywords_hashtags = pd.concat([data_en_keywords, tweets_by_hashtags], axis=0)
     return data_en_keywords_hashtags
-
-def export_data(data, path_without_filetype, pickle=True, csv=True):
-    """
-    export data in a pickle and csv format
-    :param data: DataFrame
-    :param path_without_filetype: String filepath without the extension
-    :param pickle: Bool if it should be exported as pickle
-    :param csv: Bool if it should be exported as csv 
-    :return: None
-    """
-    if pickle:
-        data.to_pickle(f'{path_without_filetype}.pkl')
-    if csv:
-        data.to_csv(f'{path_without_filetype}.csv')
 
 def preprocessing(president):
     """
@@ -116,10 +65,7 @@ def preprocessing(president):
     :return: None
     """
     # The files `LA/2018/month_2018_04.db` and `NYC/2020/month_2020_01_RADIUS.db` were deleted because they were empty or did not conform the data format of the other tables
-    root = '/Volumes/Festplatte/data-UA/'
-    data_path = root + 'data/'
-    cities = ['Birmingham', 'LA', 'London', 'NYC']
-    years = [str(year) for year in range(2018, 2023)] # ['2018', '2019', '2020', '2021', '2022']
+    (root, data_path, _, cities, years) = get_data()
     boris_johnson_hashtags = ('BorisJohnson', 'UKPrimeMinister', 'ToryLeader', 'Boris')
     donald_trump_hashtags = ('DonaldTrump', 'Trump2024', 'MakeAmericaGreatAgain', 'Trump')
     president_hashtags = donald_trump_hashtags if president == 'trump' else boris_johnson_hashtags
@@ -135,8 +81,9 @@ def preprocessing(president):
     total_iterations = len(cities) * len(years) * 12  # assuming 12 months per year
     completed_iterations = 0
     start_time = time.time()
-    all_data, X_data, y_data = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    all_data = pd.DataFrame()
     for city in cities:
+        city_data = pd.DataFrame()
         for year in years:
             for month in get_files(f'{root}{city}/{year}/'):
                 month_only = month[-5:-3]
@@ -150,13 +97,12 @@ def preprocessing(president):
                 print(f'The shape of the preprocessed data is: {month_dataset.shape}')
                 # save dataset
                 filename = f'{city}-{year}-{month_only}'
-                # save data
+                # save single month data
                 month_dataset.to_pickle(f'{data_path}{president}/{filename}.pkl')
                 # create training set with X and y
                 # save to csv for ChatGPT 4.0 to read data
-                all_data.concat(month_dataset)
-                X_data.concat(month_dataset[['text']])
-                y_data.concat(month_dataset[['Compound']])
+                all_data = pd.concat([all_data, month_dataset], ignore_index=True)
+                city_data = pd.concat([city_data, month_dataset], ignore_index=True)
                 # close the connection
                 conn.close()
 
@@ -166,11 +112,12 @@ def preprocessing(president):
                 estimated_total_time = average_time_per_iteration * total_iterations
                 estimated_time_remaining = estimated_total_time - (time.time() - start_time)
                 print(f'Estimated time remaining: {estimated_time_remaining // 60} minutes, {estimated_time_remaining % 60:.2f} seconds')
-                print('\n')    
+                print('\n')
+        export_data(city_data, f'{data_path}{president}-{city}')
     # export whole dataset
     export_data(all_data, f'{data_path}{president}')
-    export_data(X_data, f'{data_path}train/X_{president}')
-    export_data(y_data, f'{data_path}train/y_{president}')
+    export_data(all_data[['text']], f'{data_path}train/X_{president}')
+    export_data(all_data[['Compound']], f'{data_path}train/y_{president}')
 
 if __name__ == '__main__':
     preprocessing(president='trump')
